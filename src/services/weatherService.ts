71aa21f1c74fb3c6209e997f59af7d7036519d1b
justanda <miranda.justin93@gmail.com>
2024-11-11 11:31:16 -0700

    fixed env file and api key

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
-const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
 
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = process.env.REACT_APP_API_KEY;
+const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export const fetchCurrentWeather = async (city: string) => {
   try {
