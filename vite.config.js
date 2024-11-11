d7ab59a090a45b6ceaac35811f0eb1cc2f28e896
justanda <miranda.justin93@gmail.com>
2024-11-11 13:49:31 -0700

    fixed env file and api key

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 60060,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   plugins: [react()],
 });
